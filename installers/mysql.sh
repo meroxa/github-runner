60262df7a4d86a8de458f1a089a8868d76f0a00a
G. Richard Bellamy <rbellamy@terradatum.com>
2020-04-25 14:47:42 -0700

    Another attempt to get mysqld running

--- installers/mysql.sh
+++ installers/mysql.sh
@@ -18,6 +18,8 @@ apt-get install libmysqlclient-dev -y
 
 # Install MySQL Server
 MYSQL_ROOT_PASSWORD=root
+# the "/var/lib/mysql" stuff here is because the mysql-server postinst doesn't have an explicit way to disable the mysql_install_db codepath besides having a database already "configured" (ie, stuff in /var/lib/mysql/mysql)
+# also, we set debconf keys to make APT a little quieter
 {
   echo mysql-server mysql-server/data-dir select '';
   echo mysql-server mysql-server/root-pass $MYSQL_ROOT_PASSWORD '';
@@ -29,7 +31,9 @@ apt-get install -y mysql-server
 rm -rf /var/lib/mysql
 mkdir -p /var/lib/mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+# ensure that /var/run/mysqld (used for socket and lock files) is writable regardless of the UID our mysqld instance ends up having at runtime
 chmod 777 /var/run/mysqld
+# comment out a few problematic configuration values
 find /etc/mysql/ -name '*.cnf' -print0 \
   | xargs -0 grep -lZE '^(bind-address|log)' \
   | xargs -rt -0 sed -Ei 's/^(bind-address|log)/#&/'
