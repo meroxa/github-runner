825939166fe4ea0abd0195c2ec9b623ede864b9c
TJ Leonard <terrance.j.leonard@gmail.com>
2021-12-03 16:26:51 -0500

    reduce size; increase speed

---
+++ installers/google-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+################################################################################
+##  File:  google-chrome.sh
+##  Desc:  Installs google-chrome, chromedriver and chromium
+################################################################################
+
+echo "skipping"
