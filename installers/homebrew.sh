3da76df2e83620545b5b4c468e92e193aa68ac95
TJ Leonard <terrance.j.leonard@gmail.com>
2021-12-03 16:33:52 -0500

    reduce size; increase speed

---
+++ installers/homebrew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+################################################################################
+##  File:  homebrew.sh
+##  Desc:  Installs the Homebrew on Linux
+##  Caveat: Brew MUST NOT be used to install any tool during the image build to avoid dependencies, which may come along with the tool
+################################################################################
+
+echo "skipping"
