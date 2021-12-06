3cdad6c36b794a6fa579e1183cc1782f1bf1dba6
TJ Leonard <terrance.j.leonard@gmail.com>
2021-12-06 17:19:23 -0500

    add space

--- installers/grub.sh
+++ installers/grub.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"systemd.unified_cgroup_hierarchy=1/g' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"systemd.unified_cgroup_hierarchy=1 /g' /etc/default/grub
 
 update-initramfs -u -k all
 update-grub
